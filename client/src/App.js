9062fd433cc6fd9b4c7d10267902462c3d958232
Mina Samwail <minasamwail@MacBook-Air-de-Mina.local>
2021-01-13 15:23:53 +0100

    initialisation du projet

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import { Switch, Route } from "react-router-dom";
+import Home from "./views/Home";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
