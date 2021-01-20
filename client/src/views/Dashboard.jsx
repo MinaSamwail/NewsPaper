7b993b227c25d0165d7321d5f9f39270a69e7307
Mina Samwail <minasamwail@MacBook-Air-de-Mina.local>
2021-01-20 10:56:18 +0100

    add some description on the page

--- client/src/views/Dashboard.jsx
+++ client/src/views/Dashboard.jsx
@@ -10,20 +10,38 @@ class Dashboard extends React.Component {
 
   componentDidMount() {
     apiHandler.getArticle().then((data) => {
-      console.log('DATA',data.value)
-      this.setState({ articles: data.value });
+      console.log('DATA',data)
+     this.setState({ articles: data });
     });
   }
 
   render() {
     
     return (
+     
       <div>
         <h1>My Dashboard</h1>
-          {/* <DisplayArticles /> */}
+        {this.state.articles.map((article)=>{
+         return(
+           <div>
+           <img src={article.image} atl={article.title} />
+           <div>
+           <h2>{article.title}</h2>
+           <p>{article.description}</p>
+           <p>{article.author}</p>
+           <a href={article.url} target="_blank"
+            rel="noopener noreferrer">link of the article</a>
+            <button>DELETE</button>
+           </div>
+           
+        </div>
+         )
+        })}
       </div>
     )
   }
 }
 
 export default Dashboard
+
+
